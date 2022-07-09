f686fe92881a89dd9628f7c8ef61fd37cc231c16
Nguyễn Văn Linh <nguyenvanlinh041298@gmail.com>
2022-07-09 16:05:42 +0700

    fix in blog

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -5,23 +5,24 @@ import { useRouter } from "next/router";
 import Main from "src/components/Main";
 import BlogList from "src/components/Bloglist";
 import HeaderNavbar from "src/components/Headernavbar";
+import Pagination from "src/components/Pagination/Pagination";
 
-export default function Blog({ posts, datas }) {
+export default function Blog({ posts, datas, page }) {
   const router = useRouter();
-
   return (
     <div className="px-5 sm:px-0 sm:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1230px] mx-auto min-h-[90px]">
       <Main />
       <div className="mb-10">
         <HeaderNavbar posts={posts} />
         <BlogList datas={datas} />
- 
       </div>
+      <Pagination posts={posts} page={page}/>  
     </div>
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(ctx) {
+  const page = ctx.params?.page || 1;
   const response = await fetch(
     "https://api.gcosoftware.vn/wp-json/wp/v2/categories?lang=vi"
   );
@@ -34,6 +35,7 @@ export async function getStaticProps() {
     props: {
       posts: data,
       datas: posts,
+      page
     },
   };
 }
